2e34e44204d52eb59ba5a0539cb955485e84808e
AlvaroHP <alvarosepu@gmail.com>
2025-01-14 18:33:37 -0300

    fix: se corrige comando de actualizacion y se elimina archivo py deprecado

--- update_proyect.bat
+++ update_proyect.bat
@@ -2,7 +2,7 @@ REM Hacer pull de los últimos cambios
 echo Actualizando el repositorio...
 git pull origin main
 if %errorlevel% neq 0 (
-    echo Error: Falló el git pull.
+    echo Error: Falló el git pull, por favor contacte al creador.
     pause
     exit /b 1
 )
